433cb656f66bcf78524f54e35c75ccedb107fd25
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-02-15 22:34:37 +0100

    initial package structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from g_common.version import VERSION
+
+setup(name          = 'g-common',
+      version       = VERSION,
+      description   = 'g-common repository manager',
+      author        = 'Jauhien Piatlicki',
+      author_email  = 'piatlicki@gmail.com',
+      packages      = ['g_common'],
+      scripts       = ['bin/g-common'],
+      license       = 'GPL',
+      )
